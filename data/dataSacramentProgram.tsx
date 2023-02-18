1368959bf7315c3c2095d6fe9912186f8cfcfd76
syc4thbishopric <111070527+syc4thbishopric@users.noreply.github.com>
2023-02-18 15:48:28 -0700

    Update dataSacramentProgram.tsx

--- data/dataSacramentProgram.tsx
+++ data/dataSacramentProgram.tsx
@@ -1,7 +1,7 @@
 import { IProgram } from '../components/modules/sacrament-program/sacrament-program'
 
 export const dataSacramentProgram: IProgram = {
-  date: "Sunday, February 12th, 2023",
+  date: "Sunday, February 19th, 2023",
   presiding: {
     title: "Presiding",
     name: "Bishop Brian Shepherd",
@@ -14,51 +14,45 @@ export const dataSacramentProgram: IProgram = {
   },
   openingHymn: {
     title: "Opening Hymn",
-    name: "#302 I Know My Father Lives",
+    name: "#220 Lord, I Would Follow Thee",
     color: "green"
   },
   sacramentHymn: {
     title: "Sacrament Hymn",
-    name: "#183 In Rememberance of Thy Suffering",
+    name: "#187 God Loved Us, So He Sent His Son",
     color: "green"
   },
   closingHymn: {
     title: "Closing Hymn",
-    name: "#219 Because I Have Been Given Much",
+    name: "#89 The Lord Is My Light",
     color: "green"
   },
   openingPrayer: {
     title: "Opening Prayer",
-    name: "Sister Deneece Park",
+    name: "Sister Krista Riley",
     color: "yellow"
   },
   closingPrayer:  {
     title: "Closing Prayer",
-    name: "Brother Dennis Park",
+    name: "Brother Dave Riley",
     color: "yellow"
   },
   programContents: [
     {
       title: "Speaker",
-      name: "Sister Hallie Oakeson",
+      name: "High Council Speaker",
       color: "yellow",
       order: 0
     },
     {
-      title: "Speaker",
-      name: "Sister Earnest",
-      color: "yellow",
-      order: 0
+      title: "Intermediate Hymn",
+      name: "#308 Love One Another",
+      color: "green",
+      order: 1
     },
-    // {
-    //   title: "Intermediate Hymn",
-    //   name: "#6 Redeemer of Israel",
-    //   color: "green",
-    //   order: 1
-    // },
     {
       title: "Speaker",
-      name: "Sister Holman",
+      name: "High Council Speaker",
       color: "yellow",
       order: 2
     },
