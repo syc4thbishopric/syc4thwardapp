811717a5ca694d0c65abfd9057e16a879273d970
syc4thbishopric <111070527+syc4thbishopric@users.noreply.github.com>
2023-03-04 14:10:18 -0700

    Update dataSacramentProgram.tsx

--- data/dataSacramentProgram.tsx
+++ data/dataSacramentProgram.tsx
@@ -1,7 +1,7 @@
 import { IProgram } from '../components/modules/sacrament-program/sacrament-program'
 
 export const dataSacramentProgram: IProgram = {
-  date: "Sunday, February 26th, 2023",
+  date: "Sunday, March 5th, 2023",
   presiding: {
     title: "Presiding",
     name: "Bishop Brian Shepherd",
@@ -9,58 +9,58 @@ export const dataSacramentProgram: IProgram = {
   },
   conducting: {
     title: "Conducting",
-    name: "Brother Jim Crawford",
+    name: "Bishop Brian Shepherd",
     color: "red"
   },
   openingHymn: {
     title: "Opening Hymn",
-    name: "#239 Choose The Right",
+    name: "#137 Testimony",
     color: "green"
   },
   sacramentHymn: {
     title: "Sacrament Hymn",
-    name: "#193 I Stand All Amazed",
+    name: "#173 While Of These Emblems We Partake",
     color: "green"
   },
   closingHymn: {
     title: "Closing Hymn",
-    name: "#223 Have I Done Any Good?",
+    name: "#105 Master The Tempest Is Raging",
     color: "green"
   },
   openingPrayer: {
     title: "Opening Prayer",
-    name: "Brother Mike Shipley",
+    name: "Sister Allison Stringam",
     color: "yellow"
   },
   closingPrayer:  {
     title: "Closing Prayer",
-    name: "Sister Tiana Shepherd",
+    name: "Brother Zac Stringam",
     color: "yellow"
   },
   programContents: [
     {
-      title: "Speaker",
-      name: "Sister Eden Carlson",
+      title: "Fast Sunday",
+      name: "Bearing of Testimonies",
       color: "yellow",
       order: 0
     },
-    {
-      title: "Speaker",
-      name: "Brother Mike Burr",
-      color: "yellow",
-      order: 1
-    },
-    {
-      title: "Musical Number",
-      name: "Jodi Cornwall Acc Leslie Pearson, \"Where Can I Turn for Peace?\"",
-      color: "green",
-      order: 2
-    },
-    {
-      title: "Speaker",
-      name: "Brother Dan Tolbert",
-      color: "yellow",
-      order: 3
-    },
+    // {
+    //   title: "Speaker",
+    //   name: "Brother Mike Burr",
+    //   color: "yellow",
+    //   order: 1
+    // },
+    // {
+    //   title: "Musical Number",
+    //   name: "Jodi Cornwall Acc Leslie Pearson, \"Where Can I Turn for Peace?\"",
+    //   color: "green",
+    //   order: 2
+    // },
+    // {
+    //   title: "Speaker",
+    //   name: "Brother Dan Tolbert",
+    //   color: "yellow",
+    //   order: 3
+    // },
   ]
 }
