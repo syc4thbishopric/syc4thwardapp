d92fe9afbfd57d88a0ba63d59dcaa9525d26f588
syc4thbishopric <111070527+syc4thbishopric@users.noreply.github.com>
2023-03-25 10:30:31 -0600

    Update BannerCard.tsx

--- components/modules/cards/BannerCard.tsx
+++ components/modules/cards/BannerCard.tsx
@@ -29,25 +29,22 @@ const options = {
 
 const BannerCard = ({ ...card }: IBannerCard) => {
   return (
-    <div className="bg-green-50 shadow-xl rounded-lg border-2 border-green-500">
+    <div className="bg-white shadow-xl rounded-lg border-2">
       <div className="px-4 py-5 sm:p-6">
         <div className="sm:flex sm:items-start sm:justify-between">
           <div>
-            <h3 className="text-lg leading-6 font-bold text-gray-900">{card.title}</h3>
-            <div className="mt-1 pr-2 flex items-center">
-              <Icon name="calendar" className="h-4 w-4 mr-2 mb-0.5 text-gray-400" />
-              <time>{card.subtitle}</time>
-            </div>
+            <h3 className="text-base font-semibold leading-6 text-gray-900">{card.title}</h3>
+            <div className="text-sm text-primary">{card.subtitle}</div>
+            <Linkify tagName="p" options={options} className="bg-white mt-2 text-sm text-gray-500 rounded-xl">{card.paragraph && <p>{card.paragraph}</p>}</Linkify>
           </div>
           {card.button.text && (
             <div className="mt-5 sm:mt-0 sm:ml-6 sm:flex-shrink-0 sm:flex sm:items-center">
-            <PrimaryButton type="dark" color="green" link={card.button.link}>
+            <PrimaryButton size="sm" type="dark" color="primary" link={card.button.link}>
               {card.button.text}
             </PrimaryButton>
           </div>
           )}
         </div>
-        <Linkify tagName="p" options={options} className="bg-white border border-gray-200 mt-3 text-base p-5 rounded-xl">{card.paragraph && <p>{card.paragraph}</p>}</Linkify>
       </div>
     </div>
   )
