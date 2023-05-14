72fafba69d536c42469149c75c49b827d695fdbb
syc4thbishopric <111070527+syc4thbishopric@users.noreply.github.com>
2023-05-14 15:06:05 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -10,4 +10,11 @@ module.exports = withPWA({
     env: {
         LDW_KEY: process.env.LATTER_DAY_API_TOKEN,
     },
+    typescript: {
+        // !! WARN !!
+        // Dangerously allow production builds to successfully complete even if
+        // your project has type errors.
+        // !! WARN !!
+        ignoreBuildErrors: true,
+      },
 })
